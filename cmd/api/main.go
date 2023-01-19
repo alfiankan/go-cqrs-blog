806ca97a3a497b660676956f3ada494d5006ea7d
alfiankan <alfiankan19@gmail.com>
2023-01-19 07:47:36 +0700

    fix(log):uncompleted log message

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -87,7 +87,7 @@ func main() {
 	// Start server
 	go func() {
 		if err := e.Start(fmt.Sprintf(":%s", cfg.HTTPApiPort)); err != nil && err != http.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server")
+			e.Logger.Fatal("shutting down the server", err.Error())
 		}
 	}()
 
